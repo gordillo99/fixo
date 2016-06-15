86e58c17e8c9fd4259939bc6e7a5f273964794c7
Jose Gordillo <gordillo@uvic.ca>
2016-06-15 01:24:38 -0700

    added date picker. added loader for .style files (loads everything without changing the naem)

--- src/components/SetupForm/SetupForm.js
+++ src/components/SetupForm/SetupForm.js
@@ -1,17 +1,24 @@
 import React, { Component, PropTypes } from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import classNames from 'classnames';
-import { FormGroup, ControlLabel, FormControl, Button, Jumbotron } from 'react-bootstrap';
+import { FormGroup, ControlLabel, FormControl, Button, Jumbotron, ButtonGroup } from 'react-bootstrap';
+import DatePicker from './../DatePicker';
 import s from './SetupForm.css';
 
 export default class SetupForm extends Component {
 
   constructor(){
     super();
+    let today = new Date();
     this.state = {
       address: '',
-      date: ''
+      date: today.getDate() + '/' + (today.getMonth() + 1) + '/' + today.getFullYear(),
+      morning: true
     };
+  }
+
+  _handleDayChange(date) {
+    this.setState( { date: date } );
   }
 
   render() {
@@ -27,9 +34,27 @@ export default class SetupForm extends Component {
                 <FormGroup controlId="formControlsTextarea">
                   <FormControl componentClass="textarea" placeholder="Detalles sobre el trabajo" />
                 </FormGroup>
-                <Button type="submit">
-                  Aceptar
-                </Button>
+                <div className={classNames(s.datePicker)}>
+                  <DatePicker
+                    dayChange={this._handleDayChange.bind(this)}
+                  />
+                </div>
+                <div className={classNames(s.centralizedDiv)}>
+                  <div className={classNames(s.morningAfternoonButtons)}>
+                    <ButtonGroup>
+                      <Button disabled={this.state.morning}>Mañana</Button>
+                      <Button disabled={!this.state.morning}>Tarde</Button>
+                    </ButtonGroup>
+                  </div>
+                  <div className={classNames(s.dateDesc)}>
+                    <p>
+                      Ha seleccionado el {this.state.date} en la { (this.state.morning) ? 'mañana' : 'tarde' }
+                    </p>
+                  </div>
+                  <Button type="submit" className={classNames(s.acceptBtn)}>
+                    Aceptar
+                  </Button>
+                </div>
               </form>
             </div>
           </div>

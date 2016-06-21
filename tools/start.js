d11ad868a15afe1848965fc2da5f10903483a0e6
Jose Gordillo <gordillo@uvic.ca>
2016-06-20 22:56:45 -0700

    trying running build first

--- tools/start.js
+++ tools/start.js
@@ -24,7 +24,7 @@ const DEBUG = !process.argv.includes('--release');
  * synchronizing URLs, interactions and code changes across multiple devices.
  */
 async function start() {
-  await run(clean);
+  //await run(clean);
   await run(copy.bind(undefined, { watch: true }));
   await new Promise(resolve => {
     // Patch the client-side bundle configurations

8c42a479391109136feba1894106e13ddcdd2bf4
Jose Gordillo <gordillo@uvic.ca>
2016-06-20 23:05:48 -0700

    getting desperate

--- tools/start.js
+++ tools/start.js
@@ -16,6 +16,7 @@ import runServer from './runServer';
 import webpackConfig from './webpack.config';
 import clean from './clean';
 import copy from './copy';
+import build from './build';
 
 const DEBUG = !process.argv.includes('--release');
 
@@ -25,6 +26,8 @@ const DEBUG = !process.argv.includes('--release');
  */
 async function start() {
   //await run(clean);
+  process.argv.push('--release');
+  await run(require('./build'));
   await run(copy.bind(undefined, { watch: true }));
   await new Promise(resolve => {
     // Patch the client-side bundle configurations

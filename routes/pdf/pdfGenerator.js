5b8e12b921a28677d79a0df20dfc9cec18b36072
Jose Gordillo <gordillo@uvic.ca>
2016-07-19 22:42:46 -0700

    added info to generated pdf file

--- routes/pdf/pdfGenerator.js
+++ routes/pdf/pdfGenerator.js
@@ -6,11 +6,12 @@ var PDFDocument = require('pdfkit');
 router.route('/pdfGenerator/:id')
 
 	.get(function(req, res){
-		console.log(req.query);
+		var query = req.query;
 	  var doc = new PDFDocument();
 	  var filename = __dirname + '/temp/' + req.params.id + (new Date().getTime() + '.pdf');
-	  var content = 'waaaazaaaaa';
+	  var content = `ID de propuesta: ${query.id}\nNombre de usuario: ${query.u_firstname} ${query.u_lastname}\nFecha propuesta: ${query.prop_date}\nTeléfono de usuario: ${query.phone}\nEmail de usuario: ${query.email}\nDirección: ${query.address}\nÁrea: ${query.area}\nFecha de creación: ${query.created_at}\nID de fixer: ${query.fixer_id}\nNombre de fixer: ${query.f_firstname} ${query.f_lastname}\n`;
 
+	  console.log(content);
 	  res.setHeader('Content-type', 'application/pdf');
 	  res.setHeader('Access-Control-Allow-Origin', '*');
 	  res.setHeader('Content-Disposition', 'inline; filename=proposal_id_'+req.params.id+'.pdf');

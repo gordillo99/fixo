a872c6f809dd7f88dc570fe1f1409563e2c51996
Jose Gordillo <gordillo@uvic.ca>
2016-08-11 20:41:23 -0700

    decreased height of about us carousel

--- src/components/AboutUsCarousel/AboutUsCarousel.js
+++ src/components/AboutUsCarousel/AboutUsCarousel.js
@@ -1,7 +1,7 @@
 import React, { Component, PropTypes } from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import cx from 'classnames';
-import { Carousel, Jumbotron } from 'react-bootstrap';
+import { Carousel, Jumbotron, Image } from 'react-bootstrap';
 import guatemalaImage from './images/guate-night.jpg';
 import programmingImage from './images/programming.jpg';
 import fixoWhite from './images/fixo-white.png';
@@ -19,7 +19,7 @@ export default class AboutUsCarousel extends Component {
 			    <Item>
 			    	<div className={s.centralizedDiv}>
 			    		<h1 className={s.header}>Operando orgullosamente en Guatemala</h1>
-			      	<img className={s.carouselImage} width={900} height={500} alt='Guatemala' src={guatemalaImage}/>
+			      	<Image className={s.carouselImage} width={800} height={400} alt='Guatemala' src={guatemalaImage}/>
 			      </div>
 			      <Caption>
 			        <img width={190} height={165} alt='fixo' src={fixoWhite} />
@@ -28,7 +28,7 @@ export default class AboutUsCarousel extends Component {
 			    <Item>
 			    	<div className={s.centralizedDiv}>
 			    		<h1 className={s.header}>Usamos tecnología de punta</h1>
-			      	<img className={s.carouselImage} width={900} height={500} alt='Programming' src={programmingImage}/>
+			      	<Image className={s.carouselImage} width={800} height={400} alt='Programming' src={programmingImage}/>
 			      </div>
 			      <Caption>
 			        <img width={190} height={165} alt='fixo' src={fixoWhite} />

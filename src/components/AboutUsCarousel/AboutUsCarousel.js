090033d62fdd0d7a34f8b052478f8ea988be93e8
Jose Javier Gordillo <gordillo@uvic.ca>
2016-10-09 16:08:27 -0700

    left carousel on mobile and static image on desktop

--- src/components/AboutUsCarousel/AboutUsCarousel.js
+++ src/components/AboutUsCarousel/AboutUsCarousel.js
@@ -7,7 +7,7 @@ import programmingImage from './images/programming.jpg';
 import gardeningImage from './images/gardening.jpg';
 import entrepreneurImage from './images/emprendimiento.jpg';
 import fixoWhite from './images/fixo-white.png';
-import tools from './images/tools.jpg';
+import tools from './images/banner.png';
 import s from './AboutUsCarousel.css';
 import $ from 'jquery';
 
@@ -23,7 +23,47 @@ export default class AboutUsCarousel extends Component {
     return (
       <div className={cx(s.root)}>
       	<div className={s.leftAligned}>
-					<img className={s.stripeImage} src={tools} />
+					<img className={cx(s.stripeImage, s.mobileInvisible)} src={tools} />
+					<div className={s.mobileVisible}>
+						<Carousel interval={5000} pauseOnHover={false}>
+							<Item>
+								<div className={s.centralizedDiv}>
+									<h1 className={s.header}>Operando orgullosamente en Guatemala</h1>
+									<Image className={s.carouselImage} width={700} height={400} alt='Guatemala' src={guatemalaImage}/>
+								</div>
+								<Caption>
+									<img width={180} height={145} alt='fixo' src={fixoWhite} />
+								</Caption>
+							</Item>
+							<Item>
+								<div className={s.centralizedDiv}>
+									<h1 className={s.header}>Ayudamos a fixers a encontrar más trabajo</h1>
+									<Image className={s.carouselImage} width={700} height={400} alt='Gardening' src={gardeningImage}/>
+								</div>
+								<Caption>
+									<img width={180} height={145} alt='fixo' src={fixoWhite} />
+								</Caption>
+							</Item>
+							<Item>
+								<div className={s.centralizedDiv}>
+									<h1 className={s.header}>Somos emprendimiento social</h1>
+									<Image className={s.carouselImage} width={700} height={400} alt='Emprendimiento' src={entrepreneurImage}/>
+								</div>
+								<Caption>
+									<img width={180} height={145} alt='fixo' src={fixoWhite} />
+								</Caption>
+							</Item>
+							<Item>
+								<div className={s.centralizedDiv}>
+									<h1 className={s.header}>Usamos tecnología de punta</h1>
+									<Image className={s.carouselImage} width={700} height={400} alt='Programming' src={programmingImage}/>
+								</div>
+								<Caption>
+									<img width={180} height={145} alt='fixo' src={fixoWhite} />
+								</Caption>
+							</Item>
+						</Carousel>
+					</div>
 				</div>
       </div>
     );

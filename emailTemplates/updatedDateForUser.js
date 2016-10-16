4b4d454e29e8da465ec6a2a609c41eccd988013e
Jose Javier Gordillo <gordillo@uvic.ca>
2016-10-16 16:12:06 -0700

    updating emails for dates

--- emailTemplates/updatedDateForUser.js
+++ emailTemplates/updatedDateForUser.js
@@ -1,10 +1,11 @@
 module.exports = {
   createUpdateDateEmail: function(proposal, selectedDate, selectedTime) {
         var emailTemplate = '';
-        emailTemplate += '<h1 style="text-align:left">¡Tu cotización con fixo ha sido confirmada!</h1>';
-        emailTemplate += `<h4 style="text-align:left">Hemos contactado a tu fixer y la fecha ha sido confirmada.</h4>`;
-        emailTemplate += `<h4 style="text-align:left">${proposal.f_firstname} ${proposal.f_lastname} ha confirmado que llegará a ${proposal.address} el día ${selectedDate} a la hora ${selectedTime}</h4>`
-        emailTemplate += '<h4 style="text-align:left">¡Gracias por usar fixo!</h4>';
+        emailTemplate += '<h1 style="text-align:left">¡Tu servicio ha sido confirmado!</h1>';
+        emailTemplate += `<p style="text-align:left">Hemos contactado a tu fixer y ha escogido la fecha.</p>`;
+        emailTemplate += `<p style="text-align:left">${proposal.f_firstname} ${proposal.f_lastname} llegará a ${proposal.address} el día ${selectedDate} a la hora ${selectedTime}</p>`
+        emailTemplate += '<p style="text-align:left">Cualquier duda o comentario, contáctanos a fixo.comercial@gmail.com</p>';
+        emailTemplate += '<p style="text-align:left">¡Gracias por usar fixo!</p>';
         return emailTemplate;
     }
 };
